e86dab0726f9ce5f33f3219e11e8334976b37589
aswinraj <aswinrajkorkandy@gmail.com>
2022-02-05 13:35:16 +0530

    new convertor

--- App.js
+++ App.js
@@ -1,14 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react,{useState} from 'react';
+import {Alert, StyleSheet, Text, View,TextInput,Button } from 'react-native';
 
 export default function App() {
-  return (
+  const[dot,setdot]=useState("");
+  const[tod,settod]=useState('');
+
+  const onChange=(show)=>{
+    var faren ="";
+    var kelvin ="";
+    console.warn(show);
+    if (show==""){
+      setdot("");
+      settod("");
+
+    }else {
+      try {
+        faren =Number(show)*1.8+32
+        kelvin =Number(show)+273.15;
+      }catch (err){
+        console.warn(err);
+      }
+      setdot(kelvin.toString());
+      settod(faren.toString());
+
+      }
+    };  
+    
+    return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>celcius</Text>
+      <TextInput placeholder='value' onChangeText={onChange}/>
+      <Text>kelvin</Text>
+      <TextInput placeholder='value' value={dot} onChangeText={onChange}/>
+      <Text>fahrenheit</Text>
+      <TextInput placeholder='value' value={tod}/>
+
+      <Button title='convert' onPress={()=>Alert.alert('text')}/>
+      
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
